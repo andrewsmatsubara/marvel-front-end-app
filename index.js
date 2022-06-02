efcdc0dc1eb13ea6a3f2aeb248a61f6c81aa094a
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-06-02 00:33:54 -0300

    Using different approach on ReactDOM in index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./src/App";
 import { store } from './src/redux/store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Provider store={store}>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+</Provider>);
